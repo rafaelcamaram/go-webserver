e5cacd78c2fac53bea43bf005eae258af3c4a2f9
Rafael Camara <rafaelcamaram@gmail.com>
2020-10-26 21:47:35 -0300

    Adding arrays examples

--- go-getting-started/code/main.go
+++ go-getting-started/code/main.go
@@ -11,12 +11,24 @@ const (
 )
 
 func main() {
-	increment := 1
+	// increment := 1
 
-	fmt.Println("Hello from a module, Gophers!")
-	fmt.Println(first+increment, second+increment, third+increment)
+	// fmt.Println("Hello from a module, Gophers!")
+	// fmt.Println(first+increment, second+increment, third+increment)
 
-	increment = 2
+	// increment = 2
 
-	fmt.Println(first+increment, second+increment, third+increment)
+	// fmt.Println(first+increment, second+increment, third+increment)
+
+	// Array
+	var arr [3]int
+	arr[0] = 1
+	arr[1] = 2
+	arr[2] = 3
+	fmt.Println(arr)
+	fmt.Println(arr[1])
+
+	arr2 := [3]int{1, 2, 3}
+	fmt.Println(arr2)
+	fmt.Println(arr2[1])
 }
