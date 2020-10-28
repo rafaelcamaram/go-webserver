2b4adbe43de17ed4af928f2def08f3c3bce5a534
Rafael Camara <rafaelcamaram@gmail.com>
2020-10-27 21:54:11 -0300

    Adding startServer method and creating first controller

--- main.go
+++ main.go
@@ -13,4 +13,18 @@ func main() {
 		LastName:  "McMillan",
 	}
 	fmt.Println(u)
+	port := 3000
+	_, err := startWebServer(port, 2)
+	fmt.Println(err)
+}
+
+func callback() {
+	fmt.Println("Callback")
+}
+func startWebServer(port int, numberOfRetries int) (int, error) {
+	fmt.Println("Starting the server...")
+	fmt.Println("Server started on port", port)
+	fmt.Println("Number of retries", numberOfRetries)
+
+	return port, nil
 }
--- models/user.go
+++ models/user.go
@@ -8,6 +8,18 @@ type User struct {
 }
 
 var (
-	user   []*User // Slice with pointers to User objects
+	users  []*User // Slice with pointers to User objects
 	nextID = 1
 )
+
+func GetUsers() []*User {
+	return users
+}
+
+func AddUser(u User) (User, error) {
+	u.ID = nextID
+	nextID++
+	users = append(users, &u)
+
+	return u, nil
+}
